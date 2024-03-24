ab32998520635d4bd01ff84c9985cd48e9b0c8a9
Chagas011 <washington.chagas.9@hotmail.com>
2024-03-24 12:54:17 -0300

    criando componentes e estilizando lista de tarefas

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+
+      parser: 'babel-eslint',
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'linebreak-style': 0,
+    'react/state-in-constructor': 0,
+    'no-unused-vars': 0,
+    'import/no-extraneous-dependencies': 0,
+    'jsx-a11y/control-has-associated-label': 0,
+    'import/no-duplicates': 0,
+  },
+};
